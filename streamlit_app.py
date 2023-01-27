f6e4a7e605e1ee74b631015787d2d9d394c9692d
vikasuttam88 <119361777+vikasuttam88@users.noreply.github.com>
2023-01-27 00:37:43 -0700

    Update streamlit_app.py
    Added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 
 import streamlit
 streamlit.title('My Parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 blueberry menu')
-streamlit.text('Spinatch smoothie')
-streamlit.text('Harb boilde egg ')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 blueberry menu')
+streamlit.text('🥑Spinatch smoothie')
+streamlit.text('🐔 Harb boiled egg ')
 
 
