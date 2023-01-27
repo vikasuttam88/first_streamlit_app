2cc850aa2becef00b8748244064bee31e66a4a61
vikasuttam88 <119361777+vikasuttam88@users.noreply.github.com>
2023-01-27 00:40:11 -0700

    Update streamlit_app.py
    Added data from AWS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 streamlit.title('My Parents new healthy diner')
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 blueberry menu')
@@ -7,5 +8,11 @@ streamlit.text('🥑Spinatch smoothie')
 streamlit.text('🐔 Harb boiled egg ')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
+
+
+
 
 
